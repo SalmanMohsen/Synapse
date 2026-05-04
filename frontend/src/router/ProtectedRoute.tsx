cb1bde9759dc4417fc2cbeb6f695cd6aa21c28fd
Salman Mohsen <salman.mohsen88@gmail.com>
2026-05-04 17:05:32 +0300

    add ProtectedRoute

---
+++ frontend/src/router/ProtectedRoute.tsx
@@ -0,0 +1,17 @@
+import { type ReactNode } from 'react'
+import { Navigate } from 'react-router-dom'
+import { useAuthStore } from '@/features/auth/store/authSlice'
+
+interface Props {
+  children: ReactNode
+}
+
+export default function ProtectedRoute({ children }: Props) {
+  const user = useAuthStore((s) => s.user)
+
+  if (!user) {
+    return <Navigate to="/login" replace />
+  }
+
+  return <>{children}</>
+}
