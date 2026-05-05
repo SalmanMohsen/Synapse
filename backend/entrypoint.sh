bc9f81a73c4da7804afcd07935abe11be7f21a35
Salman Mohsen <salman.mohsen88@gmail.com>
2026-05-05 15:40:15 +0300

    Fix the migration issue
    -Add a mechanism in the entrypoint script to wait for the postgres to be fully ready
    -change the asyncpg library version from 0.29.0 to 0.30.0

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 set -e
+
+echo "Waiting for Postgres..."
+
+until python -c "import socket; s=socket.socket(); s.connect(('postgres', 5432)); s.close()" 2>/dev/null; do
+  sleep 1
+done
 echo "Running Alembic migrations..."
 alembic upgrade head
 echo "Starting server..."
